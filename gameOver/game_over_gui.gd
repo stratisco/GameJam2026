e833bc66f64fe1558ef17b2239a884c029950357
ryan <ryancowan55555@gmail.com>
2026-07-25 15:27:38 +0800

    added level timer, end game screen, bomb sprite, and edited 1 character in font (because it looked like a seven)

---
+++ gameOver/game_over_gui.gd
@@ -0,0 +1,36 @@
+extends Control
+
+
+@onready var attempts_label: Label = $Attempts
+
+
+@onready var fail_screen: Control = $Fail
+@onready var succeed_screen: Control = $Succeed
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	attempts_label.text = 'Attempt: %d' % globalVars.totalAttemps
+	
+	fail_screen.visible = false
+	succeed_screen.visible = false
+	
+	
+	if globalVars.didTheyWin:
+		succeed_screen.visible = true
+	else:
+		fail_screen.visible = true
+
+
+
+func _on_try_again_button_pressed() -> void:
+	# res://scenes/main.tscn
+	get_tree().change_scene_to_file("uid://dwcwaornvynca")
+
+
+func _on_play_again_pressed() -> void:
+	globalVars.didTheyWin = false
+	globalVars.totalAttemps = 0
+	
+	# res://scenes/main.tscn
+	get_tree().change_scene_to_file("uid://dwcwaornvynca")
