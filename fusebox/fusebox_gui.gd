0cc0eef08d300b14d4c39cb689666e9c4b922d98
Tom Pickering <80679736+GnirekcipMot111@users.noreply.github.com>
2026-07-26 20:10:32 +0800

    fusebox puzzle

--- fusebox/fusebox_gui.gd
+++ fusebox/fusebox_gui.gd
@@ -1,32 +1,41 @@
 extends Node2D
 
 @onready var wires_container: Node2D = $Wires
+@onready var area2d: Area2D = $Area2D
 
 
 
 var wires: Array[Node] = []
-
+var wirescut = []
 
 func _ready() -> void:
 	wires = wires_container.get_children()
-	
-	for i in wires.size():
-		var wire = wires[i]
-		wire.wire_cut.connect(_on_wire_cut.bind(i))
 
+	for i in wires.size():
+		var wire := wires[i]
+		wire.wire_cut.connect(_on_wire_cut.bind(i))
+		
 
 func _on_wire_cut(i: int):
+	
 	print("wire" + str(i))
-	if (i == 3):
-		pass # correct wire cut
+	if [4, 5, 6, 7].has(i):
+		wirescut.append(i)
+		if (wirescut.size() == 4):
+			globalVars.fuseboxWireCut.emit()
+			exit()
 	else:
 		globalVars.game_over_signal.emit()
+		print("game over")
+		return
 		
-		
+	
 
+func exit():
+	globalVars.currentGUI = null;
+	queue_free()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("interact") && globalVars.currentGUI == self:
 		get_viewport().set_input_as_handled()
-		globalVars.currentGUI = null;
-		queue_free()
+		exit()
